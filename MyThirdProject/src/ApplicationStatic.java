05114fb8e1123e658c855bb33b00c5eb1c41f8f9
pawarsonal01998 <pawarsonal01998@gmail.com>
2022-09-15 18:06:18 +0530

    first commit

--- MyThirdProject/src/ApplicationStatic.java
+++ MyThirdProject/src/ApplicationStatic.java
@@ -7,16 +7,23 @@ public class ApplicationStatic
  ApplicationStatic(int id,String p)
  {
 	 lid=id;
-	 String passward=p;
+	 passward=p;
 	 hitCounter++; 
+ }
+ public String toString()
+ {
+	return " "+ lid +" "+ passward +" "; 
  }
  
 	public static void main(String[] args) 
 	{
 		ApplicationStatic a1,a2,a3;
 		a1=new ApplicationStatic(1,"sonal@100");
+		System.out.println(a1);
 		a2=new ApplicationStatic(2,"sonalPawar");
+		System.out.println(a2);
 		a3=new ApplicationStatic(3,"sonbhi");
+		System.out.println(a3);
 		System.out.println(ApplicationStatic.hitCounter);
 	}
 
